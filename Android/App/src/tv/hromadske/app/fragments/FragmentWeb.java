e37b109b25ff24d986281066cb7871370a112395
chebTS <chebTS@gmail.com>
2013-12-03 04:39:16 +0200

    Play videos Youtube works

--- Android/App/src/tv/hromadske/app/fragments/FragmentWeb.java
+++ Android/App/src/tv/hromadske/app/fragments/FragmentWeb.java
@@ -1,11 +1,14 @@
 package tv.hromadske.app.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class FragmentWeb extends Fragment {
 	private String url;
@@ -22,9 +25,22 @@ public class FragmentWeb extends Fragment {
 		wv.getSettings().setBuiltInZoomControls(true);
 		wv.getSettings().setSupportZoom(true);
 		wv.getSettings().setUseWideViewPort(true);
+		wv.getSettings().setJavaScriptEnabled(true);
 		wv.getSettings().setLoadWithOverviewMode(true);
+		wv.setWebViewClient(new CustomWebViewClient());
 		wv.loadUrl(url);
 		return wv;
 	}
 
+	private class CustomWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			if (url.contains("hromadske.tv")) {
+				view.loadUrl(url);
+			} else {
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+			}
+			return true;
+		}
+	}
 }

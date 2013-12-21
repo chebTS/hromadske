9c23e41eb37780d627d6841bfb87e400d0dcacd8
chebTS <chebTS@gmail.com>
2013-12-21 02:21:08 +0200

    Key

--- Android/App/src/tv/hromadske/app/utils/SystemUtils.java
+++ Android/App/src/tv/hromadske/app/utils/SystemUtils.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 
 public class SystemUtils {
-	public static final String DEVELOPER_KEY = "AIzaSyAn9DDnBL7i6Chq2myHvU3YQEGVnGtDJlA";
+	public static final String DEVELOPER_KEY = "AIzaSyAaJn6jFeISxXx1qPH4I2Kfk1_pDJABGfk";
+	/*dev AIzaSyAn9DDnBL7i6Chq2myHvU3YQEGVnGtDJlA
+	 * public static final String DEVELOPER_KEY = "AIzaSyAn9DDnBL7i6Chq2myHvU3YQEGVnGtDJlA";
+	 */
 	public static final int EXIT_DELAY = 2000;
 	public static final String UKR_URL = "ukr_url";
-	
 
 	public static boolean isAppInstalledOrNot(String uri, Context context) {
 		PackageManager pm = context.getPackageManager();

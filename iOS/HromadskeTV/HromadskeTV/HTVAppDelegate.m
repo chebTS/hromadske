0d5856adb4341bcaecc3bd22cf6325d087bfff9c
Max Tymchiy <samback@ukr.net>
2014-01-16 09:07:43 +0200

    Fix bug with update when user open from background app.

--- iOS/HromadskeTV/HromadskeTV/HTVAppDelegate.m
+++ iOS/HromadskeTV/HromadskeTV/HTVAppDelegate.m
@@ -77,6 +77,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+    [self youtubePath];
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 

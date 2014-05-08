fd3b3abb29eb20ea99418c0f6d0a75606f75ecc6
comonitos <comonitos@gmail.com>
2014-05-08 18:18:24 +0300

    - update

--- iOS/HromadskeTV/HromadskeTV/Helper/HTVHelperMethods.h
+++ iOS/HromadskeTV/HromadskeTV/Helper/HTVHelperMethods.h
@@ -14,14 +14,14 @@
 
 + (void)callCustomAlertWithMessage:(NSString *)message;
 
-+ (NSString *)youtubeLiveLinkTail;
-//+ (void)saveYoutubeLiveLinkTail:(NSString *)newLink;
-//+ (NSString *)fullYoutubeLink;
 + (NSArray *)parseDictionaryFromYoutube:(NSDictionary *)dictionary;
 + (void)fetchNewDataFromYoutubeForController:(YoutubeViewController *)controller;
 + (NSString *)yotubeTailFromString:(NSString *)string;
 + (NSArray *)parseArrayFromTwitter:(NSArray *)twittes;
 
+//+ (NSString *)youtubeLiveLinkTail;
+//+ (void)saveYoutubeLiveLinkTail:(NSString *)newLink;
+//+ (NSString *)fullYoutubeLink;
 //+ (void)clearLiveLinks;
 //+ (void)saveHromadskeOnlineWithParameters:(NSDictionary *)parameters chanel:(NSString *)key;
 //+ (VideoStream *)onlineStreamForKey:(NSString *)key;
